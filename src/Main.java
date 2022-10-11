12de21416c0354e633ee85ea83f75e2b76c0eb9c
david <davidm676@nycstudents.net>
2022-10-11 18:36:24 -0400

    idk

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,17 @@ public class Main {
         int l = str1.length();
         String fh = str1.substring(0, l/2);
         String sh = str1.substring(l/2);
-        System.out.println("String len: "+l);
+        System.out.println("String length: "+l);
         System.out.println("First half: "+fh);
         System.out.println("Second half: "+sh);
 
-        System.out.println("Enter another String: ");
+        System.out.print("Enter another String: ");
         String str2 = scan.nextLine();
+        int l2 = str2.length();
 
-        if (l > str2.length())  {
-            System.out.println(str1+ "is longer");
-        } else if (l==str2.length()){
+        if (l > l2)  {
+            System.out.println(str1+ " is longer");
+        } else if (l==l2){
             System.out.println("Both strings have the same length");
         } else {
             System.out.println(str2+" is longer");
@@ -27,10 +28,16 @@ public class Main {
         if (x==0) {
             System.out.println("Both strings have the same characters");
 
-        } else if (x > 0) {
-            System.out.println(str1+" is longer")
+        } else if (x < 0) {
+            System.out.println(str1+" is first alphabetically");
+        } else {
+            System.out.println(str2+" is first alphabetically");
         }
-
+        if (str1.contains(str2)) {
+            System.out.println(str2 + " is found in " + str1 + " at index " + str1.indexOf(str2));
+        } else {
+            System.out.println(str2+ " is NOT found in " + str1);
+        }
 
     }
 }
